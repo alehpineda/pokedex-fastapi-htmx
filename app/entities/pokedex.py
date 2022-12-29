873e2b2d54b70ef3a5ef9aa7e0633313bf541713
Alex Pineda <ale.hpineda@gmail.com>
2022-12-28 20:58:35 -0600

    index

--- app/entities/pokedex.py
+++ app/entities/pokedex.py
@@ -3,5 +3,5 @@ from sqlmodel import Field, SQLModel
 
 class Pokedex(SQLModel, table=True):
     id: int = Field(primary_key=True)
-    name: str
+    name: str = Field(index=True)
     api_response: str
