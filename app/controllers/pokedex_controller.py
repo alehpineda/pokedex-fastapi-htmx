92cc50f0d660ca7b0be9cb5bf3222f56e2330eb4
Alex Pineda <ale.hpineda@gmail.com>
2022-12-29 23:42:19 -0600

    added loguru

--- app/controllers/pokedex_controller.py
+++ app/controllers/pokedex_controller.py
@@ -1,15 +1,18 @@
 from fastapi import APIRouter, HTTPException
 from services.pokedex_service import PokedexService
+import logging
 
 router = APIRouter(
     prefix="/pokedex",
     tags=["pokedex"],
 )
 
+logger = logging.getLogger(__name__)
 
 @router.get("/name/{name}")
 async def get_pokemon_by_name(name: str) -> dict:
     try:
+        logger.info(f"Get pokemon {name} info")
         return PokedexService.get_pokemon_by_name(name=name)
     except Exception:
         raise
@@ -18,6 +21,7 @@ async def get_pokemon_by_name(name: str) -> dict:
 @router.get("/id/{id}")
 async def get_pokemon_by_id(id: int) -> dict:
     try:
+        logger.info(f"Get pokemon id-{id} info")
         return PokedexService.get_pokemon_by_id(id=id)
     except Exception:
         raise
--- app/main.py
+++ app/main.py
@@ -1,11 +1,27 @@
+import logging
+from utils.custom_logging import CustomizeLogger
 from controllers import pokedex_controller
 from fastapi import FastAPI
+from pathlib import Path
 
-app = FastAPI()
+
+logger = logging.getLogger(__name__)
+
+config_path=Path("utils/logging_config.json")
+
+app = FastAPI(
+    title="Pokedex|Fastapi|HTMX",
+    debug=True
+)
+
+logger = CustomizeLogger.make_logger(config_path)
+
+app.logger = logger
 
 app.include_router(pokedex_controller.router)
 
 
 @app.get("/health")
 async def root():
+    logger.info("Pokedex App Online!")
     return {"message": "Pokedex App Online!"}
